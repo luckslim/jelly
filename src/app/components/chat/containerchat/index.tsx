b98465f06437f5573d55d4b5cf9412cd91db013d
Lucas SL <newlucaslima78@gmail.com>
2025-07-09 11:54:10 -0300

    up

--- src/app/components/chat/containerchat/index.tsx
+++ src/app/components/chat/containerchat/index.tsx
@@ -1,14 +1,32 @@
 import { ArrowLineRightIcon } from "@phosphor-icons/react";
 import { Profiler } from "../../profiler";
-import { ContainerChatProfiler } from "./style";
-
+import { ContainerChatMessage, ContainerChatProfiler, ContainerSendMessage, ContainerTextArea, StyleOffCanvas } from "./style";
+import { useState } from "react";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Actions } from "../../actions";
 export function ListChat() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <>
-            <ContainerChatProfiler>
-                <Profiler name="Lucas Soares" urlImage="https://avatars.githubusercontent.com/u/95627552?v=4" message="ola tudo bem!"/>
+            <ContainerChatProfiler onClick={handleShow}>
+                <Profiler name="Lucas Soares" urlImage="https://avatars.githubusercontent.com/u/95627552?v=4" message="ola tudo bem!" />
                 <ArrowLineRightIcon size={22} weight="thin" />
             </ContainerChatProfiler>
+            <StyleOffCanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Profiler name="Lucas Soares" urlImage="https://avatars.githubusercontent.com/u/95627552?v=4" userName="@Lucassoarelim22" />
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <ContainerChatMessage></ContainerChatMessage>
+                    <ContainerSendMessage>
+                        <ContainerTextArea/>
+                        <Actions title="enviar" />
+                    </ContainerSendMessage>
+                </Offcanvas.Body>
+            </StyleOffCanvas>
         </>
     )
 }
--- src/app/components/chat/containerchat/style.ts
+++ src/app/components/chat/containerchat/style.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import Offcanvas  from "react-bootstrap/Offcanvas";
 export const ContainerChatProfiler = styled.div`
     display: flex;
     gap: 0.5rem;
@@ -8,7 +8,22 @@ export const ContainerChatProfiler = styled.div`
     grid: 1;
     padding: 1rem;
     border-radius: 8px;
+    cursor: pointer;
     &:hover{
         background-color: ${props=>props.theme['gray-900']};
     }
+`;
+export const StyleOffCanvas = styled(Offcanvas)`
+    background-color:${props=>props.theme['gray-800']};
+    color: ${props=>props.theme['gray-300']};
+`;
+export const ContainerChatMessage = styled.div``;
+export const ContainerSendMessage = styled.div``;
+export const ContainerTextArea = styled.textarea`
+    background-color: ${props=>props.theme['gray-900']};
+    color: ${props=>props.theme['gray-300']};
+    border-color: ${props=>props.theme['gray-700']};
+    border-radius: 4px;
+    width: 100%;
+    height: 100px;
 `;
